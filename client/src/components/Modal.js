c46f9c3baab5bb9860a57f26b9d266707d174b9d
jacquelineb <jacquelinesotheremail@gmail.com>
2022-06-05 05:53:07 -0700

    Added vertical scroll to modal when content gets too large. Disable scroll on main body when modal is open

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '../styles/Modal.module.scss';
 
 function Modal({ display, handleClose, children }) {
+  useEffect(() => {
+    if (display) {
+      document.body.style.overflowY = 'hidden';
+    } else {
+      document.body.style.overflowY = 'scroll';
+    }
+    return () => (document.body.style.overflowY = 'scroll');
+  }, [display]);
+
   return display ? (
     <div className={styles.modalContainer}>
       <div className={styles.modalContent}>
