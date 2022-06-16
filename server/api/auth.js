d97034835eb925cf1464a55c162a7b20740b68c7
jacquelineb <jacquelinesotheremail@gmail.com>
2022-06-15 21:18:07 -0700

    Removed signup code

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,64 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const passport = require('passport');
-const pool = require('../db');
 const initializePassport = require('../passport-config');
 const authMiddleware = require('../utils/authMiddleware');
 
 initializePassport(passport);
-
-// Register a user
-/*
-router.post('/register', checkNotAuthenticated, async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-
-    // some validation
-    const errorMessages = {};
-    if (!username || !email || !password) {
-      errorMessages.missingFields = 'Please enter all fields.';
-    }
-
-    const existingEmail = await pool.query(
-      'SELECT id FROM blog_user WHERE LOWER(email) = LOWER($1)',
-      [email]
-    );
-    if (existingEmail.rows.length > 0) {
-      errorMessages.emailTaken = 'This email is already in use.';
-    }
-
-    const existingUsername = await pool.query(
-      'SELECT id FROM blog_user WHERE LOWER(username) = LOWER($1)',
-      [username]
-    );
-    if (existingUsername.rows.length > 0) {
-      errorMessages.usernameTaken = 'This username is taken.';
-    }
-
-    const passwordRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$');
-    if (!passwordRegex.test(password)) {
-      errorMessages.badPassword =
-        'Please make sure password is at least 8 characters, containing a mix of numbers and uppercase and lowercase letters.';
-    }
-
-    if (Object.keys(errorMessages).length) {
-      res.status(401).json(errorMessages);
-    } else {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      await pool.query(
-        'INSERT INTO blog_user (username, email, hashed_password) VALUES ($1, $2, $3)',
-        [username, email, hashedPassword]
-      );
-
-      res.status(200).json('Successfully registered user');
-    }
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json('Server error');
-  }
-});
-*/
 
 // Log a user in
 router.post(
