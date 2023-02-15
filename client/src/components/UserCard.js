312dd9b1980bdd244374c5d886a2ffd2b39ad3de
jacquelineb <jacquelinesotheremail@gmail.com>
2023-02-14 16:39:53 -0800

    Make username and avatar link back to user profile

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Avatar from './Avatar';
 import style from '../styles/UserCard.module.scss';
+import { Link } from 'react-router-dom';
 
 function UserCard({ username, bio, avatar, size = 'medium' }) {
+  const PROFILE_LINK = `/profile/${username}`;
   return (
     <div className={`${style.UserCard} ${style[size]}`}>
-      <Avatar src={avatar} alt={`${username}'s avatar`} size={size} />
+      <Link to={PROFILE_LINK}>
+        <Avatar src={avatar} alt={`${username}'s avatar`} size={size} />
+      </Link>
       <div>
-        <div className={style.username}>{username}</div>
+        <Link className={style.username} to={PROFILE_LINK}>
+          {username}
+        </Link>
         <div className={style.bio}>{bio}</div>
       </div>
     </div>
