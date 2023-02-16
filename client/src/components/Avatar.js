08362dd949b054405821d5c53bce908277db3cf3
jacquelineb <jacquelinesotheremail@gmail.com>
2023-02-15 18:13:17 -0800

    Added error check to Avatar

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -8,6 +8,10 @@ function Avatar({ src, alt, size = 'medium' }) {
       className={`${style.Avatar} ${style[size]}`}
       src={src ? src : defaultAvatar}
       alt={alt}
+      onError={(e) => {
+        e.target.src = defaultAvatar; // If error with the image src, set src to default avatar
+        e.target.onError = null; // Avoid infinite loop in case defaultAvatar also caused error
+      }}
     />
   );
 }
