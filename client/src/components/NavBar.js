822ba332b42bbef0ddc828cb949a3f86a365de87
jacquelineb <jacquelinesotheremail@gmail.com>
2023-02-15 23:38:39 -0800

    Refactored

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,73 +7,65 @@ import Icon from './Icon';
 
 function NavBar() {
   const { authUser, logout } = useAuthContext();
-  return (
-    <nav className={style.nav}>
-      {!authUser ? (
-        <div>
-          <Link className={style.link} to='/login'>
-            Log in
-          </Link>
-          <Link className={style.link} to='/signup'>
-            Sign up
-          </Link>
-        </div>
-      ) : (
-        <DropdownMenu user={authUser} logout={logout} />
-      )}
-    </nav>
-  );
-}
-
-function DropdownMenu({ user, logout }) {
-  const dropdownItemsRef = useRef(null);
-  const [dropdownIsActive, setDropdownIsActive] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownMenuRef = useRef(null);
 
   useEffect(() => {
     function handlePageClick(e) {
       // Clicking outside dropdown menu
-      if (dropdownItemsRef.current && !dropdownItemsRef.current.contains(e.target)) {
-        setDropdownIsActive(false);
+      if (dropdownMenuRef.current && !dropdownMenuRef.current.contains(e.target)) {
+        setShowDropdown(false);
       }
     }
 
-    if (dropdownIsActive) {
+    if (showDropdown) {
       window.addEventListener('click', handlePageClick);
     }
 
     return () => {
       window.removeEventListener('click', handlePageClick);
     };
-  }, [dropdownIsActive]);
+  }, [showDropdown]);
 
   return (
-    <div className={style.dropdownMenuContainer}>
-      <button
-        type='button'
-        className={style.dropdownToggle}
-        onClick={() => {
-          setDropdownIsActive(!dropdownIsActive);
-        }}
-      >
-        <span>{user}</span> <Icon name='chevronDown' size='16' />
-      </button>
+    <nav className={style.nav}>
+      {!authUser ? (
+        <>
+          <Link className={style.link} to='/login'>
+            Log in
+          </Link>
+          <Link className={style.link} to='/signup'>
+            Sign up
+          </Link>
+        </>
+      ) : (
+        <>
+          <button
+            className={style.dropdownToggle}
+            onClick={() => setShowDropdown(!showDropdown)}
+          >
+            <span>{authUser}</span> <Icon name='chevronDown' size='16' />
+          </button>
 
-      <div
-        ref={dropdownItemsRef}
-        className={`${style.dropdownContent} ${!dropdownIsActive ? style.inactive : ''}`}
-      >
-        <Link className={style.dropdownLink} to='/dashboard'>
-          Dashboard
-        </Link>
-        <Link className={style.dropdownLink} to={`/profile/${user}`}>
-          View my profile
-        </Link>
-        <CreateBlogPost />
-        <button className={`${style.logoutBtn} ${style.dropdownLink}`} onClick={logout}>
-          Log out
-        </button>
-      </div>
-    </div>
+          <div
+            className={style.dropdownMenu}
+            style={{ display: showDropdown ? 'block' : 'none' }}
+            ref={dropdownMenuRef}
+          >
+            <Link className={style.dropdownLink} to='/dashboard'>
+              Dashboard
+            </Link>
+            <Link className={style.dropdownLink} to={`/profile/${authUser}`}>
+              View my profile
+            </Link>
+            <CreateBlogPost />
+            <button className={`${style.logoutBtn} ${style.dropdownLink}`} onClick={logout}>
+              Log out
+            </button>
+          </div>
+        </>
+      )}
+    </nav>
   );
 }
 
