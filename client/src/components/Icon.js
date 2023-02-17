5567bdb2d8b841f5f2f6710056d97b64909fecf3
jacquelineb <jacquelinesotheremail@gmail.com>
2023-02-16 17:29:31 -0800

    Added Icon for creating new post.

--- client/src/components/Icon.js
+++ client/src/components/Icon.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const ICONS = {
+  create: (
+    <>
+      <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+      <path d='M8 20l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4h4z'></path>
+      <path d='M13.5 6.5l4 4'></path>
+      <path d='M16 18h4m-2 -2v4'></path>
+    </>
+  ),
   edit: (
     <>
       <path stroke='none' d='M0 0h24v24H0z' fill='none' />
