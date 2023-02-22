717f27a37dfb17a3c210dd9afc675519057ba9f7
jacquelineb <jacquelinesotheremail@gmail.com>
2023-02-21 18:12:11 -0800

    make title link to the post with comments page

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import formatDate from '../utils/formatDate';
 import style from '../styles/BlogPost.module.scss';
+import { Link } from 'react-router-dom';
 
 function BlogPost({ post }) {
   const { id, title, body, created_on } = post;
   return (
     <div>
-      <h2 className={style.title}>{title}</h2>
+      <h2 className={style.title}>
+        <Link to={`/post/${post.id}`}>{title}</Link>
+      </h2>
       <p className={style.postMetaData}>
         Posted on <span className={style.date}>{formatDate(created_on).date}</span>
       </p>
