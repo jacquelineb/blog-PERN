7860c7f0f933c080e44dde49c2217ce1fafacc2c
jacquelineb <jacquelinesotheremail@gmail.com>
2023-03-06 23:32:01 -0800

    Remove Page layout

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import EditUserDetails from '../components/EditUserDetails';
-import Page from '../layouts/Page';
 import style from '../styles/Dashboard.module.scss';
 
 function Dashboard() {
   return (
-    <Page>
-      <Page.Main>
-        <div className={style.Dashboard}>
-          <h1>Dashboard</h1>
-          <div>
-            <EditUserDetails />
-          </div>
-        </div>
-      </Page.Main>
-    </Page>
+    <div className={style.Dashboard}>
+      <h1>Dashboard</h1>
+      <div>
+        <EditUserDetails />
+      </div>
+    </div>
   );
 }
 
