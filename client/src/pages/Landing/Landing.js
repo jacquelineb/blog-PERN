918074385ca61fc386554aa0a019f45e530a8b09
jacquelineb <jacquelinesotheremail@gmail.com>
2024-01-02 18:23:16 -0800

    Add content to landing page

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import style from './Landing.module.scss';
 
 function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div className={style.Landing}>
+      <h1>Welcome</h1>
+      <div className={style.description}>
+        <p>
+          This is a blog application I created to learn full-stack web development. It's built
+          using the PERN stack (PostgreSQL, Express, React, and Node.js)
+        </p>
+        <p>
+          You can view the source code on GitHub @{' '}
+          <a
+            href='https://github.com/jacquelineb/pern-stack-blog'
+            target='_blank'
+            rel='noreferrer'
+          >
+            https://github.com/jacquelineb/pern-stack-blog
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
